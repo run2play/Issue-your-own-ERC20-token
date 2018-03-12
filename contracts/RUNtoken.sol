5803d2a736859523a5303dc4543e7b4e2d446d3a
run2play <35581831+run2play@users.noreply.github.com>
2018-03-12 11:20:25 -0400

    Update and rename erc20_tutorial.sol to RUNtoken.sol

--- contracts/RUNtoken.sol
+++ contracts/RUNtoken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.18;
 // '0Fucks' token contract
 //
 // Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
+// Symbol      : RUN
 // Name        : 0 Fucks Token
 // Total supply: 100000000
 // Decimals    : 18
